e4d1d5ef202c100ad758b6146273ce4cf9e88ba2
fratello@Ohimesama <fratello@Ohimesama.localdomain>
2025-06-14 02:21:44 +0700

    add recipe and ingredient models

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=128, unique=True,
+                            verbose_name='Название')
+    measurement_unit = models.CharField(
+        max_length=64, unique=True, verbose_name='Единица измерения')
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+
+    def __str__(self):
+        return f'{self.name}, {self.measurement_unit}'
+
+
+class Recipe(models.Model):
+    # author =
+    name = models.CharField(max_length=128, verbose_name='Название')
+    image = models.ImageField(verbose_name='Картинка',
+                              upload_to='recipes/images/')
+    text = models.CharField(verbose_name='Описание')
+    # ingredients = models.ManyToManyField(
+    #     Ingredient, verbose_name='Список ингредиентов',
+    #     through='Recipe_Ingredient')
+    cooking_time = models.IntegerField(
+        verbose_name='Время приготовления (в минутах)',
+        validators=[MinValueValidator(1)])
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
