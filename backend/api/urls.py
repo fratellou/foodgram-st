044f421e856626dfca08d22c4225a64213dabdc4
fratello@Ohimesama <fratello@Ohimesama.localdomain>
2025-06-14 19:02:35 +0700

    add ingredientViewSet

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,13 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from .views import IngredientViewSet
 
 app_name = 'api'
 
 router = DefaultRouter()
+router.register(
+    r'ingredients', IngredientViewSet, basename='ingredients')
+
 
 urlpatterns = [
     path('', include(router.urls)),
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import IngredientSerializer
+from recipes.models import Ingredient
+from rest_framework.permissions import AllowAny
+from rest_framework.pagination import PageNumberPagination
+from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 6
+    page_size_query_param = 'limit'
+    page_query_param = "page"
+    max_page_size = 50
+
+
+class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+    filter_backends = [DjangoFilterBackend]
+    permission_classes = [AllowAny]
+    search_fields = ['^name']
+    pagination_class = None
